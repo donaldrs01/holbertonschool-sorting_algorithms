a3a1cd78106a863dad5b54e2ce64e814c52a1b63
Your Name <you@example.com>
2023-12-14 12:25:10 -0800

    added sort flag

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,23 +3,30 @@
 /**
  * selection_sort - sorts array of integers using selection sort
  * @array: array of integers to be sorted in ascend
- * @sizee: size of array
+ * @size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
 	int temp;
+	int sortflag;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i; /* assume current index is min */
+		sortflag = 1;
 
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
+				sortflag = 0;
 			/* finds minimum element in unsorted part */
 		}
+	}
+
+	if (sortflag)
+		break;
 
 		temp = array[i];
 		array[i] = array[min_index];
