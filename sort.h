f1d5c1dbd9a83173e787ebcf0ecd10e3cf3d72e6
Your Name <you@example.com>
2023-12-14 11:28:59 -0800

    added Big O notes

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* function declarations */
@@ -25,4 +25,4 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 
-#endif 
+#endif
