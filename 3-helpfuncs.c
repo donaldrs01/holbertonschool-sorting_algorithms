88456d6bbcdf324132bc1aefe6aa2fb7be2bb728
Your Name <you@example.com>
2023-12-18 13:50:48 -0800

    merged subfunction into same file with quicksort

--- 3-helpfuncs.c
+++ 3-helpfuncs.c
@@ -48,24 +48,3 @@ int lomuto(int *array, int low, int high, size_t size)
 	swap(array, i, high, size);
 	return (i);
 }
-
-/**
- * quick_sort_help: performs quicksort on array
- * @array: array of integers
- * @low: index of low number
- * @high: index of high number
- * @size: size of array
- */
-
-void quick_sort_help(int *array, int low, int high, size_t size)
-{
-	int pivot;
-
-	if (low >= high)
-	{
-		return;
-	}
-	pivot = lomuto(array, low, high, size);
-	quick_sort_help(array, low, pivot - 1, size);
-	quick_sort_help(array, pivot +1, high, size);
-}
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include "sort.h"
 /**
+ * quick_sort_help: performs quicksort on array
+ * @array: array of integers
+ * @low: index of low number
+ * @high: index of high number
+ * @size: size of array
+ */
+
+void quick_sort_help(int *array, int low, int high, size_t size)
+{
+        int pivot;
+
+        if (low >= high)
+        {
+                return;
+        }
+        pivot = lomuto(array, low, high, size);
+        quick_sort_help(array, low, pivot - 1, size);
+        quick_sort_help(array, pivot +1, high, size);
+}
+
+/**
  * quick_sort - sorts array of integers using the quick sort method
  * @array: the integer array to be sorted in ascending order
  * @size: the size of the array
  */
+
 void quick_sort(int *array, size_t size)
 {
 	if (size < 2)
